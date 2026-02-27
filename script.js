d924aeab3fffbbfd41045427c2aedda488bd7128
bhumikashirke68-creator <bhumikashirke68@gmail.com>
2026-02-27 10:04:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+const counter = document.getElementById("counter");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+
+increaseBtn.addEventListener("click", () => {
+    count++;
+    counter.textContent = count;
+});
+
+decreaseBtn.addEventListener("click", () => {
+    count--;
+    counter.textContent = count;
+});
+
+resetBtn.addEventListener("click", () => {
+    count = 0;
+    counter.textContent = count;
+});
